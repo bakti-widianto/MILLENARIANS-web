d9d0c1968f8daa52e4e6187d2695219a6179a60f
bakti <bakti@vcgamers.com>
2023-10-20 13:41:37 +0700

    final update

---
+++ assets/data/data.js
@@ -0,0 +1,100 @@
+const myJson = [
+  {
+    id: "art_dance",
+    name: "Art Dance",
+    desc: [
+      "Dance is a rhythmic movement performed in a certain place and time to express a feeling and convey a message from a person or group",
+      "Art is a form of human self-expression that is often used as a means of entertainment and performance.",
+      "We show various dance arts in each country, like Japanese, Indonesian, Italian, and Rajasthani.",
+      "Performed by: Nazwa K, Sarah N, Indah Puspita, Sifa A, Yuniarti, Alifa S, Syaira A, Farah, Siti Fadlah, Putri F, Sarah S, Muslichah, Indri N, Syakila N, Khayla D, Natasya M, Lulu S, Alifa C, Sheilla Putri Azizah, Siti Linda,",
+    ],
+  },
+  {
+    id: "fashion-show",
+    name: "Fashion Show",
+    desc: [
+      "A Fashion show is an event put on by a fashion designer to showcase their upcoming line of clothing and/or accessories during a fashion show",
+      "In A typical fashion show, models walk the catwalk dressed in the clothing created by the designer. Clothing is illuminated on the catwalk using lighting and special effects",
+      "We use costumes from various countries like Camboja, Thailand, Korea, Philipine Performed by: Rafeyfa, Tiara, Nabila T, Razita, Faza, Alisha, Nabila J",
+    ],
+  },
+  {
+    id: "musical-drama",
+    name: "Musical Drama",
+    desc: [
+      "A musical drama that could be considered an experimental musical drama. We didn't have the supplies from the theater to make it. We have the intention of creating a musical drama that has never been performed before.",
+      "Our musical drama has a story with an irregular plot. We imitate the same concept, namely by creating a plot with scenes that are not connected at all.",
+      "More precisely, this is similar to a public service advertisement made into a musical performance.",
+      "We chose the theme of the impact of the digital era for this musical drama. and this is really worth watching for all groups.",
+    ],
+  },
+  {
+    id: "cabaret",
+    name: "Cabaret",
+    desc: [
+      "Cabaret is a performance or artistic performance originating from the Western world where there is usually entertainment in the form of music, comedy, and often skits or dances.",
+      "In this chance we will show about “Daily Activity In Boarding School”. It tells the story of the twists and turns of life, togetherness, and kinship,",
+      "We present you with a show that is worthy and also very enjoyable because there are lots of comedy acts performed by the performers",
+    ],
+  },
+  {
+    id: "dance",
+    name: "Dance",
+    desc: [
+      "Dance is a performing art form consisting of sequences of movement, either improvised or purposefully selected. This movement has aesthetic and often symbolic value",
+      "This time our dancer will show feature dances from the Jabbawockez and Kinjaz, which are well-known dance groups. This performance will be very satisfying with its choreography we hope you all enjoy the show.",
+      "This performance will be performed by: Giza, Yahya, Jess, Regan, Rendy, Dwi P, Alfi, Rafif",
+    ],
+  },
+  {
+    id: "flashmob",
+    name: "Flashmob",
+    desc: [
+      "Flash mob is defined as a group of people who gather in the same place and at the same time, perform dance movements, this will be performed by all of class 6 , to mark the our big event will be end with dancing together",
+      "We will perform a dance from the “Terhebat” song from our boyband Cowboy Junior with his dance that raises enthusiasm to all audiences",
+    ],
+  },
+  {
+    id: "magic-show",
+    name: "Magic Show Trick",
+    desc: [
+      `A magic show is a theatrical act that a magician performs in front of an audience to amaze and entertain them, this chance our Magician “ Iqbal “ will show you a “Card Tricks Magic Show”`,
+      "This type of magic show requires much more people-participation, and like general close-up magic, the magician is an expert manipulating and creating many magic routines and magical effects with cards",
+    ],
+  },
+  {
+    id: "stickman",
+    name: "Stickman Dance",
+    desc: [
+      "Stickman is an LED dance performed by four person they are, Agung Cahya, Abizar Ahmad, Dyral Setiawan, and Ahmad Hilmi.",
+      "The song that will be performed is  Eye of the Tiger - Survivor, Gangnam Style - PSY, Lazy Song - Bruno Mars, Waka-Waka - Shakira, Big Boy, Barbie Girl – Aqua, and Bumble Bee.",
+      "Stickman dance is evolving from a dance that makes a variable choreograph with an LED concept. There are many unique choreograph from us.",
+    ],
+  },
+  {
+    id: "music-performance",
+    name: "Music Performance",
+    desc: [
+      "Music performance is a band performance. There are six personnel, they are Aditya Farrel as vocal, Albar Awaludin as bassist, Azmi Ahsani as lead guitar, Reinard Addzikra as keyboardist, Adha Muhammad as rhythm guitar, and Rizqi Widianto as drummer.",
+      "Aditya Farrel is the only member who made some songs. His first single the title is “LASKAR (Aksi Seni dan Kreasi)” The song was released for OST the scout competition in Al Ihsan. The song is available on various music platforms",
+      "We performed 4 songs, they are Separuh - NOAH, Duka - Last Child, Seluruh Nafas Ini - Last Child, and Sky Full of Stars - Cold Play. There is some salutation from us the title is “Mughrom” for beginning our performance.",
+    ],
+  },
+  {
+    id: "barongsai",
+    name: "Barongsai Performance",
+    desc: [
+      "The Chinese lion dance is referred to as barongsai in Indonesia, often performed by Chinese Indonesian during Imlek. Indonesians however, have developed their own style of lion dances. Barongsai is performed by Ferdi, Hikmal and Abiel, Aji.",
+      "The lion dance (Indonesian: barong) in Indonesia has different forms that are distinct to the local cultures in Indonesia, and it is not known if these have any relation to the Chinese lion. The best known lion dances are performed in Bali and Java.",
+      "Never before had anyone performed a Barongsai at Al Ihsan Boarding School. And we are the first who perform the barongsai. So, enjoy our show!",
+    ],
+  },
+  {
+    id: "poeth",
+    name: "Poeth Perfomance",
+    desc: [
+      "This is our prologue to begin our story, poeth performed by Adam and Nazrul. They are explaining about our show from the start until the end",
+      "Poeth is describing about the revolution of technology modernity. And describe about history our performance, its seems like history og barongsai, Band Etc",
+    ],
+  },
+];
